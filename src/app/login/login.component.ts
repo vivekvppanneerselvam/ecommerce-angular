2a1613a020edc86cd04e50b7ad77cebfb9a85885
vivekvppanneerselvam <vivekvppanneerselvam@gmail.com>
2018-10-05 00:33:07 +0530

    login/register functionality update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,31 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators,FormControl,AbstractControl} from '@angular/forms';
 import {RegisterService} from '../service/register.service';
+import {AuthenticationService} from '../service/authentication.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  providers: [RegisterService]  
+  providers: [RegisterService, AuthenticationService]  
 })
 export class LoginComponent implements OnInit {
   registrationForm: FormGroup;
+  loginForm: FormGroup;
   formData:any;
   emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  constructor(private fb: FormBuilder,private registerService: RegisterService, private router: Router) { }
+  flag :boolean = false;
+
+  constructor(private fb: FormBuilder,private registerService: RegisterService, private authService: AuthenticationService, private router: Router) { }
 
   ngOnInit() { 
+    this.loginForm = this.fb.group({
+      custPassword: ['', Validators.required],
+      custUserId: ['', Validators.required],
+      //checkBoxValue: [false, Validators.pattern('true')],
+    });
+
     this.registrationForm = this.fb.group({
-      firstName: ['', [Validators.required, Validators.pattern(/^([a-zA-Z]+[,.]?[0-9]?[ ]?|[a-zA-Z]+['-]?)+$/)]],
-      lastName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z '.-]*$/)]],
-      companyName:['', [Validators.required]],
-      phoneNumber:['', [Validators.required,Validators.minLength(14)]],      
+      custFirstName: ['', [Validators.required, Validators.pattern(/^([a-zA-Z]+[,.]?[0-9]?[ ]?|[a-zA-Z]+['-]?)+$/)]],
+      custLastName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z '.-]*$/)]],
+      custPhoneNum:['', [Validators.required,Validators.minLength(14)]],      
       passwords: this.fb.group({
       password:['', [Validators.required,Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)]],
       confirmPassword:['']
       },{validator: this.checkPasswords}),
-      email: ['', [Validators.required,Validators.pattern(this.emailRegex)],[this.emailExisteValidation.bind(this)]]
+      custEmailAddr: ['', [Validators.required,Validators.pattern(this.emailRegex)],[this.emailExisteValidation.bind(this)]]
     });
     
   }
@@ -55,6 +64,52 @@ export class LoginComponent implements OnInit {
     gotoPasswordRecoveryPage= function () {
       this.router.navigateByUrl('/password-recovery');
     };
+
+    loginCheck(){
+      if(!this.loginForm.invalid){
+         //this.loadingService.display(true);
+        this.authService.login(this.loginForm.get('custUserId').value, this.loginForm.get('custPassword').value).subscribe(
+        res =>{
+          //this.loadingService.display(false);
+          if(!res){
+            this.flag= true;
+          }
+          this.router.navigate(['/home']);
+        },error => {
+          //this.loadingService.display(false);
+          this.flag= true;
+          this.router.navigate(['/home']);
+          //this.toasterService.pop((error.status == 200) ? 'success':'error',error._body);
+        })
+       }
+     }
+
+
+    sendRegistrationForm(){
+      if(this.registrationForm.valid){
+     //  this.loaderService.display(true);
+       var obj ={
+        custFirstName: this.registrationForm.get('custFirstName').value ,
+        custLastName: this.registrationForm.get('custLastName').value  ,        
+        custPhoneNum:(this.registrationForm.get('custPhoneNum').value).replace(/[()\s-_]/g, ""),
+        passwords: this.registrationForm.get('passwords').value,
+        custEmailAddr: this.registrationForm.get('custEmailAddr').value
+       }
+        this.registerService.addRegistrationFormDetails(obj).subscribe(
+        response =>{
+      //  this.loaderService.display(false);
+        console.log("Registration status :: "+response.text());
+      //  this.toasterService.pop((response.status == 200) ? 'success':'error',response.text());
+        this.ngOnInit();
+        },
+        error => {
+       // this.loaderService.display(false);
+        console.log("Error"+error);
+     //   this.toasterService.pop('error',error._body);
+        }
+      );
+      }
+    }
 
 
 }
--- src/app/service/register.service.ts
+++ src/app/service/register.service.ts
@@ -14,21 +14,21 @@ addRegistrationFormDetails(formdata: any): Observable<any> {
             var headers = new Headers();
             headers.append('Content-Type', 'application/json');
             let options = new RequestOptions({headers : headers});
-            return this.http.post('/fleetvansftl/register/createuser', formdata, options)
+            return this.http.post('/register/createuser', formdata, options)
             .pipe(map(this.extractData),
             catchError(this.handleErrorObservable));
 
   }
 
   checkEmail(emailAddress : any) : Observable<any> {
-    return this.http.get('/fleetvansftl/register/emailExist'+"?emailAddress="+emailAddress)
+    return this.http.get('/register/emailExist'+"?emailAddress="+emailAddress)
     .pipe(map(this.extractData),catchError(this.handleErrorObservable));
   }
 
 
 
   usernameExist(username : any) : Observable<any> {
-    return this.http.get('/fleetvansftl/register/usernameExist'+"?username="+username)
+    return this.http.get('/register/usernameExist'+"?username="+username)
     .pipe(map(this.extractData),
     catchError(this.handleErrorObservable));
   }
