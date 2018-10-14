0afd326353eb4f2e563315b6f7fd226be9a306ba
vivekvppanneerselvam <vivekvppanneerselvam@gmail.com>
2018-10-14 14:25:08 +0530

    changes for admin page

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -11,17 +11,19 @@ export class AdminComponent implements OnInit {
   ngOnInit() {
   }
   columnDefs = [
-    {headerName: 'Make', field: 'make' },
-    {headerName: 'Model', field: 'model' },
-    {headerName: 'Price', field: 'price'}
-];
+    { headerName: 'Product Id', field: 'productId' },
+    { headerName: 'Product Name', field: 'productTitle' },
+    { headerName: 'Category', field: 'productMainCategory' },
+    { headerName: 'Sub-Category', field: 'productSubCategory' },
+    { headerName: 'Product Price', field: 'productPrice' },
+    { headerName: 'Product Discount', field: 'productDiscount' },
+    { headerName: 'Product Weight', field: 'productWeight' },
+    { headerName: 'Product Status', field: 'productStatus' }   
+  ];
 
-rowData = [
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 }
-];
-
-  
+  rowData = [
+    { productId: 'Toyota', productTitle: 'dsfa', productMainCategory: 35000 , productSubCategory: 'dsfa',  productPrice: 'dsfa', 
+      productDiscount: 'dsfa',  productWeight: 'dsfa', productStatus:''},
+  ];
 
 }
--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IconCamera, IconHeart, IconGithub, IconShoppingCart, IconUser, IconMenu, IconHelpCircle, IconXCircle,IconXSquare, IconCheckSquare, IconCheckCircle, IconBell, IconArrowLeftCircle, IconArrowLeft, IconArrowRightCircle, IconArrowRight, IconSettings,  IconShoppingBag,  IconAward,  IconTruck,  IconHome, IconCheck, IconInstagram, IconTwitter, IconFacebook, IconMapPin, IconTag, IconX } from 'angular-feather';
+import { IconCamera, IconHeart, IconGithub, IconShoppingCart, IconUser, IconMenu, IconHelpCircle, IconXCircle,IconXSquare, IconCheckSquare, IconCheckCircle, IconBell, IconArrowLeftCircle, IconArrowLeft, IconArrowRightCircle, IconArrowRight, IconSettings,  IconShoppingBag,  IconAward,  IconTruck,  IconHome, IconCheck, IconInstagram, IconTwitter, IconFacebook, IconMapPin, IconTag, IconX, IconRefreshCw } from 'angular-feather';
 const icons = [
   IconCamera,
   IconHeart,
@@ -29,7 +29,8 @@ const icons = [
   IconFacebook,
   IconMapPin,
   IconTag,
-  IconX
+  IconX,
+  IconRefreshCw
 
 ];
 @NgModule({
